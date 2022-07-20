54136cfbb2194d9076a883687e73ff446e3065b1
natnael-lema <88472250+natnael-lema@users.noreply.github.com>
2022-07-20 14:22:18 +0530

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,12 +2,13 @@ import axios from "axios";
 const LOGIN_USER_KEY = "LOGIN_USER_KEY";
 
 var baseURL;
-if (
-  process.env.REACT_APP_ENVIRONMENT &&
-  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
-) {
-  baseURL = process.env.REACT_APP_API_BASE_URL;
-} else baseURL = "https://tea-shop1-backend.herokuapp.com";
+// if (
+//   process.env.REACT_APP_ENVIRONMENT &&
+//   process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+// ) {
+//   baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else 
+  baseURL = "https://tea-shop1-backend.herokuapp.com";
 
 const api = axios.create({
   baseURL: baseURL,
